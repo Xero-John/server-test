5fd6aac2c6546557d83c9539ed0cff284e371e57
Xero-John <xerojohn7@gmail.com>
2023-12-25 22:31:38 +0800

    initial

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello!',
-  })
+app.get('/accounts', async (req, res) => {
+    const hello = await prisma.accounts.findMany();
+    res.json(hello);
 })
-app.get('/account', (req, res) => {
-  res.json({
-    message: 'account!',
-  })
+
+app.get('/', async (req, res) => {
+    const hello = await prisma.employees.findMany();
+    res.json({
+        message: 'Employees API!',
+        "employees": hello
+    });
 })
-app.get('/thanks', (req, res) => {
-  res.json({
-    message: 'thanks!',
-  })
-})
+
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'Hello!',
+//   })
+// })
+// app.get('/account', (req, res) => {
+//   res.json({
+//     message: 'account!',
+//   })
+// })
+// app.get('/thanks', (req, res) => {
+//   res.json({
+//     message: 'thanks!',
+//   })
+// })
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
