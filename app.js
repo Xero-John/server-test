6d50fcb6143df8b1be485d9b2a19e89c8e599242
Xero-John <xerojohn7@gmail.com>
2023-12-26 14:15:42 +0800

    initial

--- app.js
+++ app.js
@@ -1,25 +1,97 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const {PrismaClient} = require('@prisma/client');
+const {PrismaClient, accounts} = require('@prisma/client');
 const prisma = new PrismaClient();
+const account = app.get('/accounts/:username&:password', async (req, res) => {
+    const username = req.params.username;
+    const password = req.params.password;
 
-app.get('/accounts', async (req, res) => {
-    const input = req.query.input;
-    console.log(input);
-    const hello = await prisma.accounts.findMany();
-    res.json(hello);
-})
-
-app.get('/', async (req, res) => {
-    const hello = await prisma.employees.findMany();
-    res.json({
-        message: 'Employees API!',
-        "employees": hello
+    const data = await prisma.accounts.findMany({
+        where: {
+            username: username,
+            password: password
+        },
+        include: {
+            employees: {
+                include: {
+                    department: true,
+                    job_class: true
+                }
+            }
+        }
     });
+
+    res.json(data);
+    return data.map(d => d.employee_id).toString()
+
 })
+
+app.get('/attendance/:accountId', async (req, res) => {
+    const accountId = parseInt(req.params.accountId);
+    const accountWithEmployeeAndAttendance = await prisma.accounts.findUnique({
+        where: {
+            account_id: accountId // specify the ID of the account you want to retrieve
+        },
+        include: {
+            employees: {
+                select: {
+                    picture: true,
+                    rfid: true,
+                    first_name: true,
+                    last_name: true,
+                    middle_name: true,
+                    suffix: true,
+                    gender: true,
+                    age: true,
+                    email: true,
+                    phone_no: true,
+                    birthdate: true,
+                    working_type: true,
+                    hired_date: true,
+                    status_: true, // 'status' might be a reserved word, so use 'status_' or change the field name if necessary
+                    isEmployed: true,
+                    job_class: {
+                        select: {
+                            job_title: true
+                        }
+                    },
+                    attendance: {
+                        select: {
+                            attendance_id: true,
+                            date: true,
+                            time_in: true,
+                            time_out: true,
+                            shift_length: true,
+                            over_time: true,
+                            under_time: true,
+                            late_duration: true,
+                            status: true
+                        }
+                    },
+                    department: {
+                        select: {
+                            department_name: true
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    console.log(accountWithEmployeeAndAttendance);
+
+    res.json(accountWithEmployeeAndAttendance);
+
+})
+
+app.get("/", (req, res) => {
+
+    res.json({message: "ok"});
+});
+
 
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
